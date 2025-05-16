e16a746eda8d4dd8e28cb737be3d6a89afe748e8
Anthony Molina Meriño <admm1225@gmail.com>
2025-05-16 13:05:46 -0500

    New color

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -2,12 +2,12 @@ import { Cat } from "lucide-react";
 
 export default function About() {
   return (
-    <div className="col-start-1 row-start-1 col-span-2 md:col-start-1 md:row-start-1 md:col-span-2 md:row-span-1 p-10 rounded-xl text-[#48a48b] bg-[#015e43]">
+    <div className="col-start-1 row-start-1 col-span-2 md:col-start-1 md:row-start-1 md:col-span-2 md:row-span-1 p-10 rounded-xl text-[#c56c1a] bg-[#F9C595]">
       <div className="flex items-center space-x-2">
-        <Cat className="text-[#48a48b]" />
+        <Cat />
         <h2 className="text-xl font-bold uppercase tracking-[3px]">About</h2>
       </div>
-      <p className="text-xl font-normal mt-4 text-white">
+      <p className="text-xl font-normal mt-4">
         Hey! I’m DevTony, a Frontend Developer and Computer Engineer who turns
         caffeine into sleek interfaces. Welcome to my portfolio, where
         everything works… probably.
