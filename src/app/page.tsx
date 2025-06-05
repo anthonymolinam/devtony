79926a9f5d60b1daf70f83418283491cb934d805
Anthony Molina Meriño <admm1225@gmail.com>
2025-06-05 12:36:01 -0500

    New card color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,25 @@ export default function Home() {
       <Hero />
       {/* Añadida la clase w-full al div del grid */}
       <div className="grid grid-cols-1 md:grid-cols-3 grid-rows-6 md:grid-rows-2 gap-4 md:gap-4 my-4 w-full ">
-        <div className="col-start-1 row-start-1 md:col-start-1 md:row-start-1 md:col-span-1 md:row-span-1 bg-gray-300 rounded-xl p-10 flex items-center justify-center h-[245px]">
+        <div className="col-start-1 row-start-1 md:col-start-1 md:row-start-1 md:col-span-1 md:row-span-1 bg-[#202120] border-3 border-[#2d2c2d] rounded-2xl p-10 flex items-center justify-center h-[245px]">
           0
         </div>
-        <div className="col-start-1 row-start-2 md:col-start-2 md:row-start-1 md:col-span-1 md:row-span-1 bg-gray-300 rounded-xl p-10 flex items-center justify-center">
+        <div className="col-start-1 row-start-2 md:col-start-2 md:row-start-1 md:col-span-1 md:row-span-1 bg-[#202120] border-3 border-[#2d2c2d] rounded-2xl p-10 flex items-center justify-center">
           1
         </div>
-        <div className="col-start-1 row-start-3 md:col-start-3 md:row-start-1 md:col-span-1 md::row-span-1 bg-gray-300 rounded-xl p-10 flex items-center justify-center">
+        <div className="col-start-1 row-start-3 md:col-start-3 md:row-start-1 md:col-span-1 md::row-span-1 bg-[#202120] border-3 border-[#2d2c2d] rounded-2xl p-10 flex items-center justify-center">
           2
         </div>
-        <div className="col-start-1 row-start-4 md:col-start-1 md:row-start-2 md::col-span-1 md::row-span-1 bg-gray-300 rounded-xl p-10 flex items-center justify-center">
+        <div className="col-start-1 row-start-4 md:col-start-1 md:row-start-2 md::col-span-1 md::row-span-1 bg-[#202120] border-3 border-[#2d2c2d] rounded-2xl p-10 flex items-center justify-center">
           3
         </div>
-        <div className="col-start-1 row-start-5 row-span-2 md:col-start-2 md:row-start-2 md:col-span-2 md:row-span-1 bg-gray-300 rounded-xl p-10 flex items-center justify-center">
+        <div className="col-start-1 row-start-5 row-span-2 md:col-start-2 md:row-start-2 md:col-span-2 md:row-span-1 bg-[#202120] border-3 border-[#2d2c2d] rounded-2xl p-10 flex items-center justify-center">
           Projects
         </div>
       </div>
+      <footer className="w-full text-center py-4">
+        Coded with 💖 by DevTony
+      </footer>
     </div>
   );
 }
