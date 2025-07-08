3b9d612b02d65b95a18b96a4063628337e8a6489
Anthony Molina Meriño <admm1225@gmail.com>
2025-07-08 13:33:26 -0500

    Better design to the music component

--- src/app/components/music.tsx
+++ src/app/components/music.tsx
@@ -6,21 +6,24 @@ import Link from "next/link";
 
 export default function Music() {
   return (
-    <div className="col-start-1 row-start-2 md:col-start-2 md:row-start-1 md:col-span-1 md:row-span-1 bg-[#202120] border-3 border-[#2d2c2d] rounded-2xl p-10 flex items-center justify-center h-[245px] w-[265px] md:w-[245px]">
-      <div className="w-[260px] text-white text-center">
-        {/* Texto superior */}
-        <p className="text-sm text-gray-300 mb-3">My music playlist</p>
+    <div className="relative bg-[#202120] border-3 border-[#2d2c2d] rounded-2xl p-6 h-[245px] w-[265px] md:w-[245px] flex flex-col justify-center items-center">
+      {/* Texto esquinado */}
+      <p className="absolute top-5 left-6 text-sm text-gray-300">
+        My music playlist
+      </p>
 
-        {/* Carrusel de portadas */}
-        <div className="relative flex justify-center items-center h-[140px] mb-4">
+      {/* Contenido central */}
+      <div className="mt-10 flex flex-col items-center justify-center">
+        {/* Carrusel */}
+        <div className="relative flex justify-center items-center mb-4">
           {/* Imagen izquierda */}
-          <div className="absolute -left-6 transform -rotate-6 scale-100 z-0">
+          <div className="absolute -left-4 -rotate-6 z-0">
             <Image
               src={playlist2}
               alt="Left Cover"
-              width={90}
-              height={90}
-              className="w-[90px] h-[90px] rounded-lg border-2 border-white object-cover"
+              width={70}
+              height={70}
+              className="rounded-lg border-2 border-white object-cover"
             />
           </div>
 
@@ -29,27 +32,30 @@ export default function Music() {
             <Image
               src={playlist1}
               alt="Center Cover"
-              width={112}
-              height={112}
-              className="w-28 h-28 rounded-xl border-2 border-white object-cover"
+              width={90}
+              height={90}
+              className="rounded-xl border-2 border-white object-cover"
             />
           </div>
 
           {/* Imagen derecha */}
-          <div className="absolute -right-6 transform rotate-6 scale-100 z-0">
+          <div className="absolute -right-4 rotate-6 z-0">
             <Image
               src={playlist3}
               alt="Right Cover"
-              width={90}
-              height={90}
-              className="w-[90px] h-[90px] rounded-lg border-2 border-white object-cover"
+              width={70}
+              height={70}
+              className="rounded-lg border-2 border-white object-cover"
             />
           </div>
         </div>
 
-        {/* Nombre del playlist */}
-        <Link href="https://music.apple.com/profile/anthonymolina">
-          <h2 className="font-semibold text-lg">Anthony Playlist</h2>
+        {/* Texto inferior */}
+        <Link
+          href="https://music.apple.com/profile/anthonymolina"
+          className="text-center"
+        >
+          <h2 className="font-semibold text-lg text-white">Anthony Playlist</h2>
           <p className="text-sm text-[#FF4E6B]">▶ Play on Apple Music</p>
         </Link>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="flex flex-col items-start min-h-screen p-4 max-w-[800px] mx-auto ">
       <Hero />
       {/* Añadida la clase w-full al div del grid */}
-      <div className="grid grid-cols-1 md:grid-cols-3 grid-rows-6 md:grid-rows-2 gap-4 md:gap-4 my-4 w-full">
+      <div className="grid grid-cols-1 md:grid-cols-3 grid-rows-5 md:grid-rows-2 gap-4 md:gap-4 my-4 w-full">
         <div className="col-start-1 row-start-1 md:col-start-1 md:row-start-1 md:col-span-1 md:row-span-1 bg-[#202120] border-3 border-[#2d2c2d] rounded-2xl p-10 flex items-center justify-center h-[245px] w-[265px] md:w-[245px]">
           0
         </div>
